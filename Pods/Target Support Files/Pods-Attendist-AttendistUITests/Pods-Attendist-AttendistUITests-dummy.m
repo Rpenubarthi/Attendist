39161ecd135631275e8fd32540bf4a38db502afc
Ruthvik Penubarthi <102499197+Rpenubarthi@users.noreply.github.com>
2024-08-09 12:01:41 -0400

    Adding project to github

---
+++ Pods/Target Support Files/Pods-Attendist-AttendistUITests/Pods-Attendist-AttendistUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Attendist_AttendistUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Attendist_AttendistUITests
+@end
---
+++ Pods/Target Support Files/Pods-AttendistTests/Pods-AttendistTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AttendistTests : NSObject
+@end
+@implementation PodsDummy_Pods_AttendistTests
+@end
